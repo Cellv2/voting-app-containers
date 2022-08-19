5ea3ae36d309cc605567ddaef3c6d9234837bda5
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2022-08-19 01:39:54 +0100

    Add keyspace notification subscriptions

--- worker/Program.cs
+++ worker/Program.cs
@@ -20,20 +20,50 @@ var redisConnectionMultiplexer = services.GetRequiredService<IConnectionMultiple
 var redisService = new RedisService(redisConnectionMultiplexer);
 
 var sync = new DatabaseSync(mongoService, redisService);
-await sync.SyncRedisIntoMongoDb();
+// await sync.SyncRedisIntoMongoDb();
+await sync.Sync();
 
 
-public class DatabaseSync {
+public class DatabaseSync
+{
     private readonly MongoDbService _mongoDbService;
     private readonly RedisService _redisService;
 
-    public DatabaseSync (MongoDbService mongoDbService, RedisService redisService) {
+    public DatabaseSync(MongoDbService mongoDbService, RedisService redisService)
+    {
         _mongoDbService = mongoDbService;
         _redisService = redisService;
     }
 
+    public async Task Sync()
+    {
+        var subs = _redisService.GetKeyspaceChannelMessageQueues();
+
+        // TODO: is there a nicer way to do this? rather than a wildcard keyspace notifiation?
+        if (subs.Channel.ToString() == "__keyspace@0__:*")
+        {
+            subs.OnMessage(async message =>
+            {
+                // keyspace notification messages follow this format: __keyspace@0__:1:incrby
+                // the channel is the first part of the message (__keyspace@0__:1), with the key itself being the second part
+                var updatedKey = message.Channel.ToString().Split(":")[1];
+                var votes = await _redisService.GetVoteCounts(updatedKey);
+                Console.WriteLine($"key {updatedKey} was updated, the new value is {votes}");
+                _mongoDbService.UpdateVoteDoc(updatedKey, votes);
+            });
+        }
+
+        while (true)
+        {
+            // TODO: make the heartbeat better somehow?
+            Console.WriteLine("Worker keep alive heartbeat");
+            await Task.Delay(TimeSpan.FromSeconds(10));
+        }
+    }
+
     // TODO: move the delay out of here
-    public async Task SyncRedisIntoMongoDb() {
+    public async Task SyncRedisIntoMongoDb()
+    {
         var counter = 0;
         var max = 100;
 
@@ -51,3 +81,4 @@ public class DatabaseSync {
         }
     }
 }
+
--- worker/Services/RedisService.cs
+++ worker/Services/RedisService.cs
@@ -6,10 +6,53 @@ namespace VotingAppContainers.Worker.Services
     public class RedisService
     {
         private readonly IConnectionMultiplexer _redis;
+        private ChannelMessageQueue _keyspaceSubscription;
 
         public RedisService(IConnectionMultiplexer redisMultiplexer)
         {
             _redis = redisMultiplexer;
+            _keyspaceSubscription = CreateKeyspaceSubscription();
+        }
+
+        private ChannelMessageQueue CreateKeyspaceSubscription()
+        {
+            ISubscriber subscriber = _redis.GetSubscriber();
+            return subscriber.Subscribe("__keyspace@0__:*");
+        }
+
+        public ChannelMessageQueue GetKeyspaceChannelMessageQueues()
+        {
+            return _keyspaceSubscription;
+        }
+
+        // TODO: constant the keyspaces and channels to make them easy to update
+        public void CreateSubscriptions2()
+        {
+            ISubscriber subscriber = _redis.GetSubscriber();
+            // subscriber.Subscribe("__keyspace@0__:*", (channel, value) =>
+            // {
+            //     if (channel.ToString() == "__keyspace@0__:1")
+            //     {
+            //         Console.WriteLine($"key 1 was updated, the new value is {value}");
+            //     }
+            //     else if (channel.ToString() == "__keyspace@0__:2")
+            //     {
+            //         Console.WriteLine($"key 2 was updated, the new value is {value}");
+            //     }
+            // });
+
+            var x = subscriber.Subscribe("__keyspace@0__:1");
+
+            subscriber.Subscribe("__keyspace@0__:1").OnMessage(message =>
+            {
+                Console.WriteLine($"key 1 was updated, the new value is {GetVoteCounts(VoteOption.One).Result}");
+                // GetVoteCounts()
+            });
+
+            subscriber.Subscribe("__keyspace@0__:2").OnMessage(message =>
+            {
+                Console.WriteLine($"key 2 was updated, the new value is {GetVoteCounts(VoteOption.Two).Result}");
+            });
         }
 
         public async Task<(string votesForOne, string votesForTwo)> GetVoteCounts()
@@ -29,6 +72,27 @@ namespace VotingAppContainers.Worker.Services
 
             var res = (votesForOne, votesForTwo);
             return res;
+        }
+
+        public async Task<string> GetVoteCounts(string option)
+        {
+            IDatabase db = _redis.GetDatabase();
+            // IServer server = _redis.GetServer(_redis.GetEndPoints()[0]);
+            // var keys = server.Keys();
+
+            // foreach (var key in keys)
+            // {
+            //     var val = db.StringGet(key);
+            //     Console.WriteLine($"{key} : {val.ToString()}");
+            // }
+
+            return (await db.StringGetAsync(option)).ToString();
+
+            // var votesForOne = await db.StringGetAsync(VoteOption.One);
+            // var votesForTwo = await db.StringGetAsync(VoteOption.Two);
+
+            // var res = (votesForOne, votesForTwo);
+            // return res;
         }
     }
 }
